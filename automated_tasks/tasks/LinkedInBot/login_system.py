2c21990c843e2eae79e28e897b9f3af1163d8408
Geno <genomags@gmail.com>
2024-02-25 14:31:10 -0500

    Added login verification and navigation checks

--- automated_tasks/tasks/LinkedInBot/login_system.py
+++ automated_tasks/tasks/LinkedInBot/login_system.py
@@ -1,4 +1,10 @@
-import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+
+from automated_tasks.subtasks.human_type import human_type
+from automated_tasks.subtasks.random_sleep import random_sleep
 
 class LinkedInLoginSystem:
     def __init__(self, session_manager, session_id):
@@ -8,19 +14,76 @@ class LinkedInLoginSystem:
         # Navigate to the LinkedIn login page
         print("Navigating to LinkedIn Login")
         self.driver.get("https://www.linkedin.com/login")
+
+        if not self.verify_login_navigation():
+            print("Failed to navigate to LinkedIn login page")
+            return False
+
+        print("Successfully navigated to LinkedIn login page")
         print("user: ", username, " ", "pass: ", password)
+        
+        # Find the username input field and enter the username(email or telephone number)
+        username_input = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//input[@id='username']"))
+        )
+        username_input.clear()
+        random_sleep(1.2, 2)
+        # Send Keys in a human-like fashion
+        human_type(username_input, username)
 
-        '''
-        # Enter login credentials and submit the form
-        # Assuming `self.driver` is a Selenium WebDriver instance
-        username_field = self.driver.find_element("id", "username")
-        password_field = self.driver.find_element("id", "password")
-        login_button = self.driver.find_element("xpath", "//button[@type='submit']")
+        # Find the password input field and enter the password
+        password_input = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//input[@id='password']"))
+        )
 
-        username_field.send_keys(username)
-        password_field.send_keys(password)
+        password_input.clear()
+        random_sleep(1.2, 2)
+        # Send Keys in a human-like fashion
+        human_type(password_input, password)
+
+        random_sleep(2,3)
+        # Find the login button and click it
+        login_button = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//button[@type='submit']"))
+        )
         login_button.click()
-        '''
 
+        random_sleep(2,3)
+
+        if not self.verify_login():
+            print("Failed to login to LinkedIn")
+            return False
         # Add any necessary logic to handle login verification
         print("Logged into LinkedIn")
+    
+    def verify_login(self):
+        try:
+            # Wait for a the feed to be present
+            linkedin_feed = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//main[@aria-label='Main Feed']"))
+            )
+            if linkedin_feed:
+                print("Logged into LinkedIn")
+            return True
+        except NoSuchElementException:
+            print("Navigation element with aria-label 'Main Feed' not found.")
+            return False
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return False
+    
+    def verify_login_navigation(self):
+        try:
+            # Wait for the login form to be present
+            login_form = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//form[@class='login__form']"))
+            )
+            if login_form:
+                print("Login form found")
+            return True
+        except NoSuchElementException:
+            print("Navigation element with aria-label 'pagination' not found.")
+            return False
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return False
